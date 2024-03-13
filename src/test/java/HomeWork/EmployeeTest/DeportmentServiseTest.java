4ed2985a29d0c5a98125f31c1141ad920bf92b9a
anatolii <anatoliise@mail.ru>
2024-03-13 09:57:02 +0300

    comit

--- src/test/java/HomeWork/EmployeeTest/DeportmentServiseTest.java
+++ src/test/java/HomeWork/EmployeeTest/DeportmentServiseTest.java
@@ -12,62 +12,68 @@ import java.util.*;
 
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class DeportmentServiseTest {
 
 
     @Mock
     EmployeeServis employeeServis;
-   // Employee employee;
+
     @InjectMocks
     DeportamentServis deportamentServis;
 
     @BeforeEach
+    void setUp() {
+        Employee employee1;
+
+        List<Employee> employee = new ArrayList<>();
+
+        employee.add(new Employee("Gary", "Potter", 445, 1));
+        employee.add(new Employee("Gary", "Potter2", 447, 2));
+        employee.add(new Employee("Pit", "Ivanov", 4575, 9));
 
 
-    void setUp(){
-Employee employee1;
-
-List<Employee> employee= new List<>();
-
-        employee.add(new Employee("Gary","Potter",445,2));
-         employee.add(new Employee("Gary","Potter2",447,2));
-         employee.add(new Employee("Pit","Ivanov",4575,9));
-
-
-
-             when(employeeServis.list()).thenReturn(employee);
+        when(employeeServis.list()).thenReturn(employee);
 
 
     }
 
     @Test
+    void deportmentTestForMaxSalary() {
+        var actual = (new Employee("Gary", "Potter2", 447, 2));
 
-    void  deportmentTestForMaxSalary(){
-       var actual=deportamentServis.maxSalaryDeportament(2);
-
-        Assertions.assertEquals(deportamentServis.maxSalaryDeportament(2), 447);
+        Assertions.assertEquals(deportamentServis.maxSalaryDeportament(2), (actual));
         Assertions.assertThrows(EmployeeNotFoundException.class, () -> deportamentServis.maxSalaryDeportament(5));
     }
-    @Test
-    void  deportmentTestForMinSalary(){
-        Assertions.assertEquals(deportamentServis.minSalary(9), 447);
-        Assertions.assertThrows(EmployeeNotFoundException.class, () -> deportamentServis.maxSalaryDeportament(5));
-        //assertThat()
-    }
-    @Test
-    void deportamentTestForFind(){
-       assertThat(deportamentServis.deportamentWorks(2)).containsExactlyInAnyOrder(
-                (new Employee("Gary","Potter",445,2)),
-                (new Employee("Gary","Potter2",447,2)));
 
-
+    @Test
+    void deportmentTestForMinSalary() {
+        var actual = (new Employee("Pit", "Ivanov", 4575, 9));
+        Assertions.assertEquals(deportamentServis.minSalary(9), (actual));
+        Assertions.assertThrows(EmployeeNotFoundException.class, () -> deportamentServis.minSalary(5));
 
     }
-    @Test
-    void findWorksDeportamentTest(){
 
-        Assertions.assertEquals(deportamentServis.maxSalaryDeportament(2), 447);
+    @Test
+    void deportamentTestForFind() {
+        var actual = deportamentServis.deportamentWorks(2);
+
+        assertThat(actual).containsExactlyInAnyOrder(
+                new Employee("Gary", "Potter", 445, 2),
+                new Employee("Gary", "Potter2", 447, 2));
+
+
+    }
+
+    @Test
+    void findWorksDeportamentTest() {
+
+        Map<Integer, Employee> map = new HashMap<>();
+        map.put(1, (new Employee("Gary", "Potter", 445, 1)));
+        map.put(2, (new Employee("Gary", "Potter2", 447, 2)));
+        map.put(9, (new Employee("Pit", "Ivanov", 4575, 9)));
+        Assertions.assertEquals(deportamentServis.findDeportamentWorks(), map);
     }
 
 }
